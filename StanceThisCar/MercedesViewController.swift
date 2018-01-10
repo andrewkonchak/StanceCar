40b9fedb6ac87889534cce1149a967af67ec8e4f
Andrew <andrew@Andrews-Mac.local>
2018-01-10 15:39:40 +0200

    Add orientation

--- StanceThisCar/MercedesViewController.swift
+++ StanceThisCar/MercedesViewController.swift
@@ -1,63 +1,54 @@
 //
-//  ViewController.swift
+//  MercedesViewController.swift
 //  StanceThisCar
 //
-//  Created by Andrew on 1/5/18.
+//  Created by Andrew on 1/6/18.
 //  Copyright © 2018 Andrew Konchak. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class MercedesViewController: UIViewController {
 
     @IBOutlet weak var constraintOutlet: NSLayoutConstraint!
     @IBOutlet weak var colfImageOutlet: UIImageView!
-    
-    var suspens = false
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     @IBAction func actionOne(_ sender: UIButton) {
-        constraintOutlet.constant = 51
+        constraintOutlet.constant = 338
         UIView.animate(withDuration: 0.5, delay: 0.1, usingSpringWithDamping: 1, initialSpringVelocity: 0.0, options: .curveEaseOut, animations: {
             self.view.layoutIfNeeded()
         }, completion: nil)
     }
     
     @IBAction func actionTwo(_ sender: UIButton) {
-        constraintOutlet.constant = 59
+        constraintOutlet.constant = 330
         UIView.animate(withDuration: 1.4, delay: 0.0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.0, options: .curveEaseOut, animations: {
             self.view.layoutIfNeeded()
         }, completion: nil)
     }
     
     @IBAction func actionThree(_ sender: UIButton) {
-        constraintOutlet.constant = 67
+        constraintOutlet.constant = 324
         UIView.animate(withDuration: 1.4, delay: 0.0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.0, options: .curveEaseOut, animations: {
             self.view.layoutIfNeeded()
         }, completion: nil)
     }
     
     @IBAction func actionJeep(_ sender: UIButton) {
-        constraintOutlet.constant = 40
+        constraintOutlet.constant = 345
         UIView.animate(withDuration: 1.4, delay: 0.0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.0, options: .curveEaseOut, animations: {
             self.view.layoutIfNeeded()
         }, completion: nil)
     }
-    
-    
-    
-    
-    
 
 }
-
